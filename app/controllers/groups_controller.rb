656aa2712a983f2e6528feb0907e8e564ef7d329
shika1993 <shikanai1993@gmail.com>
2019-10-25 14:17:30 +0900

    3rd commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,26 @@ class GroupsController < ApplicationController
     end
   
     def create
+
+      @group = Group.new(group_params)
+
+      if @group.save
+        redirect_to root_path, notice: 'グループを作成しました' 
+      else          #validationでエラーが起こった時の処理がこっち
+        render :new #renderは指定された名前のビューのみを表示
+      end
+
     end
   
     def edit
     end
   
     def update
+    end
+
+    private
+    def group_params
+      params.require(:group).permit(:name, {:user_ids => []})
     end
 
 end
