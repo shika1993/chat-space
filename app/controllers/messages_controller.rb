deeb582d663b8f8c70c72b64b2a6a4ceeb9b68e7
shika1993 <shikanai1993@gmail.com>
2019-10-28 13:01:08 +0900

    1st commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,11 @@ class MessagesController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def create
+  end
+
+  private
+
+  def message_params
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     devise_for :users
     root 'groups#index'
     resources :users, only: [:edit, :update]
-    resources :groups, only:[:new, :create, :edit, :update]
+    resources :groups, only:[:new, :create, :edit, :update] do
+      resources  :message, only:[:index, :create]
+    end
   end
 end
