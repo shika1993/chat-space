3f43a8871bb5de4eaece0a471b019e228d2c6ae8
shika1993 <shikanai1993@gmail.com>
2019-10-29 15:11:12 +0900

    4th commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,26 @@
 class MessagesController < ApplicationController
-  
+  before_action :set_group
+
+
   def index
     @name = Group.find_by(id: params[:group_id]).name
+    @message = Message.new
   end
 
   def create
+
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+ 
 
 end
