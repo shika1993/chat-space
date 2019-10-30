c78a9e2c6aa341c591d004eddf4e51d6ea736fe4
shika1993 <shikanai1993@gmail.com>
2019-10-30 16:07:07 +0900

    7th commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,18 @@ class MessagesController < ApplicationController
   def index
     @name = Group.find_by(id: params[:group_id]).name
     @message = Message.new
+    @messages = @group.messages.includes(:user) #messageテーブルの中からクリックされたグループのidと一致するレコードを取得
   end
 
   def create
-
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
   private
@@ -17,10 +25,10 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
   end
 
-  def set_group
-    @group = Group.find(params[:group_id])
-  end
 
+  def set_group
+    @group = Group.find(params[:group_id]) #クリックしたグループ固有のidを持つレコードを取得
+  end
  
 
 end
